17ad8009f079f583d0056778be2907bff741062e
hyeon <nguswjd02@gmail.com>
2025-06-16 22:26:44 +0900

    login 폼 상단 border-radius 제어

--- js/main.js
+++ js/main.js
@@ -31,3 +31,30 @@ function Errormsg(e) {
 }
 
 loginForm.addEventListener("submit", Errormsg);
+
+// 판매 회원 / 구매회원 버튼 클릭
+const buyerBtn = document.getElementById("buyer-btn");
+const sellerBtn = document.getElementById("seller-btn");
+
+function clickbuyBtn() {
+  buyerBtn.classList.remove("not-focusBtn");
+  buyerBtn.classList.add("focusBtn");
+  sellerBtn.classList.remove("focusBtn");
+  sellerBtn.classList.add("not-focusBtn");
+  
+  // js로 스타일 수정 괜찮나 ?
+  loginForm.style.borderRadius = '0 10px 10px 10px';
+}
+
+function clicksellernBtn() {
+  sellerBtn.classList.remove("not-focusBtn");
+  sellerBtn.classList.add("focusBtn");
+  buyerBtn.classList.remove("focusBtn");
+  buyerBtn.classList.add("not-focusBtn");
+
+  loginForm.style.borderRadius = '10px 0 10px 10px';
+}
+
+buyerBtn.addEventListener("click", clickbuyBtn);
+sellerBtn.addEventListener("click", clicksellernBtn);
+
