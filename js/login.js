fd0529b319081d97f8f4d81f638e1edff7ab50b2
hyeon <nguswjd02@gmail.com>
2025-06-19 10:52:29 +0900

    login errormsg js 추가

--- js/login.js
+++ js/login.js
@@ -1,48 +1,52 @@
-// login error Msg
 const loginForm = document.getElementById("loginForm");
 const userIdInput = document.getElementById("user-id");
 const userPwInput = document.getElementById("user-pw");
-const loginError = document.getElementById("login-error");
+const loginContainer = document.querySelector("#loginForm > div");
 
+function showMSG(parentElement, text, color = "#EB5757") {
+    if (parentElement.querySelector("p")) {
+        parentElement.querySelector("p").remove();
+    }
+    const msg = document.createElement("p");
+    msg.textContent = text;
+    msg.style.color = color;
+    msg.style.margin = "8px";
+    parentElement.appendChild(msg);
+}
+
+// login error Msg
 function Errormsg(e) {
-  loginError.textContent = "";
-
-  if(userIdInput.value === '' && userPwInput.value === '' || userIdInput.value === '') {
-  e.preventDefault();
-    loginError.textContent = "아이디를 입력해 주세요.";
-    userIdInput.focus();
-  } else if (userPwInput.value === '') {
-    e.preventDefault();
-    loginError.textContent = "비밀번호를 입력해 주세요.";
-    userPwInput.focus();
-  }
+    if (userIdInput.value === '') {
+        e.preventDefault();
+        showMSG(loginContainer, "아이디를 입력해 주세요.");
+        userIdInput.focus();
+    } else if (userPwInput.value === '') {
+        e.preventDefault();
+        showMSG(loginContainer, "비밀번호를 입력해 주세요.");
+        userPwInput.focus();
+    }
 }
 
 loginForm.addEventListener("submit", Errormsg);
-
 
 // 판매 회원 / 구매회원 버튼 클릭
 const buyerBtn = document.getElementById("buyer-btn");
 const sellerBtn = document.getElementById("seller-btn");
 
 function clickbuyBtn() {
-  buyerBtn.classList.remove("not-focusBtn");
-  buyerBtn.classList.add("focusBtn");
-
-  sellerBtn.classList.remove("focusBtn");
-  sellerBtn.classList.add("not-focusBtn");
-  
-  loginForm.style.borderRadius = '0 10px 10px 10px';
+    buyerBtn.classList.remove("not-focusBtn");
+    buyerBtn.classList.add("focusBtn");
+    sellerBtn.classList.remove("focusBtn");
+    sellerBtn.classList.add("not-focusBtn");
+    loginForm.style.borderRadius = '0 10px 10px 10px';
 }
 
 function clicksellernBtn() {
-  sellerBtn.classList.remove("not-focusBtn");
-  sellerBtn.classList.add("focusBtn");
-  
-  buyerBtn.classList.remove("focusBtn");
-  buyerBtn.classList.add("not-focusBtn");
-
-  loginForm.style.borderRadius = '10px 0 10px 10px';
+    sellerBtn.classList.remove("not-focusBtn");
+    sellerBtn.classList.add("focusBtn");
+    buyerBtn.classList.remove("focusBtn");
+    buyerBtn.classList.add("not-focusBtn");
+    loginForm.style.borderRadius = '10px 0 10px 10px';
 }
 
 buyerBtn.addEventListener("click", clickbuyBtn);
