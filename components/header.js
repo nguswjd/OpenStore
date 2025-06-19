917e21bab642a09c29d0ef2047ba597f1b5804fe
hyeon <nguswjd02@gmail.com>
2025-06-19 15:32:45 +0900

    header, footer 컴포넌트 파일로 뺌

---
+++ components/header.js
@@ -0,0 +1,32 @@
+document.querySelector("header").innerHTML = `
+    <section class="logo-search">
+      <h1>
+        <img src="assets/icons/Logo-hodu.png" alt="호두마켓 로고">
+      </h1>
+
+      <form class="search-box" id="search-form">
+        <input type="text" placeholder="상품을 검색해보세요!" id="search-input" name="search">
+        <button type="submit">
+          <img src="assets/icons/search.svg" alt="검색">
+        </button>
+      </form>
+    </section>
+
+    <section class="user-menu">
+      <h2 class="sr-only">장바구니 및 로그인</h2>
+      <ul>
+        <li>
+          <a href="#">
+            <img src="assets/icons/icon-shopping-cart.svg" alt="장바구니">
+            <span>장바구니</span>
+          </a>
+        </li>
+        <li>
+          <a href="login.html">
+            <img src="assets/icons/icon-user.svg" alt="로그인">
+            로그인
+          </a>
+        </li>
+      </ul>
+    </section>
+`;
