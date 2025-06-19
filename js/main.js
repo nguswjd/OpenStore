0a06ae0daffbf621a3e8feac9b53ef0f94e82db9
hyeon <nguswjd02@gmail.com>
2025-06-19 17:30:03 +0900

    상세 제품 페이지 url에 상품 id 추가

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const productContainer = document.querySelector(".product-list ul");
 
 if (location.href.includes("buyer.html")) {
   const changeSeller = document.getElementById("mypage-seller");
-  // changeBuyer.innerHTML = "";
+
   changeSeller.innerHTML = `
     <li id="mypage-seller">
       <a href="login.html">
@@ -14,11 +14,12 @@ if (location.href.includes("buyer.html")) {
         마이페이지
       </a>
     </li>
-    `;
+  `;
 } else if (location.href.includes("seller.html")) {
   const changeSeller = document.getElementById("mypage-seller");
   const changeBuyer = document.getElementById("shopping-mypage");
-  changeBuyer.innerHTML =`
+
+  changeBuyer.innerHTML = `
     <li id="mypage-seller">
       <a href="login.html">
         <img src="assets/icons/icon-user.svg" alt="로그인">
@@ -27,19 +28,21 @@ if (location.href.includes("buyer.html")) {
     </li>
   `;
   changeSeller.innerHTML = `
-    <a href = "#" id = "shopingBag">
-        <img src="assets/icons/icon-shopping-bag.svg" alt="">
-        판매자 센터
+    <a href="#" id="shopingBag">
+      <img src="assets/icons/icon-shopping-bag.svg" alt="">
+      판매자 센터
     </a>
   `;
 }
 
 // 상품추가
 function addProduct(product) {
+  if (!productContainer) return;
+
   const li = document.createElement("li");
 
   li.innerHTML = `
-    <a href="product_details.html">
+    <a href="product_details.html?id=${product.id}">
       <img src="${product.image}" alt="${product.name}">
       <p class="store-name">${product.seller.store_name}</p>
       <p class="product-name">${product.name}</p>
@@ -47,34 +50,29 @@ function addProduct(product) {
     </a>
   `;
 
-  // if (productContainer.children.length >= 6) {
-  //   li.style.display = "none";
-  // }
-
   productContainer.append(li);
 }
 
 // 상품 API 호출
-fetch(`${baseUrl}products/`)
-  .then((res) => {
-    if (!res.ok) {
-      throw new (`HTTP error! status: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then((data) => {
-    // console.log(data);
-    if (data.results && Array.isArray(data.results)) {
+if (productContainer) {
+  fetch(`${baseUrl}products/`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      if (data.results && Array.isArray(data.results)) {
         for (let i = 0; i < data.results.length; i++) {
           const product = data.results[i];
           addProduct(product);
         }
-      } 
+      }
     })
     .catch((error) => {
-      console.log("error");
+      console.log("error:", error);
     });
+}
 
-// https://shape-coding.tistory.com/entry/JavaScript-자바스크립트로-숫자-3자리천단위-마다-콤마-찍기
 
-// https://csdrive.tistory.com/22
